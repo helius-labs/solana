9698ddd7566bef6b85bdda3af885c96d9242073a
godmodegalactus <godmodegalactus@gmail.com>
2024-04-06 18:52:00 +0200

    Making collect_all_unsorted true on ScanConfig to optimize gPA requests

--- accounts-db/src/accounts_index.rs
+++ accounts-db/src/accounts_index.rs
@@ -90,7 +90,7 @@ pub enum UpsertReclaim {
     IgnoreReclaims,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ScanConfig {
     /// checked by the scan. When true, abort scan.
     pub abort: Option<Arc<AtomicBool>>,
@@ -98,6 +98,15 @@ pub struct ScanConfig {
     /// true to allow return of all matching items and allow them to be unsorted.
     /// This is more efficient.
     pub collect_all_unsorted: bool,
+}
+
+impl Default for ScanConfig {
+    fn default() -> Self {
+        Self {
+            collect_all_unsorted: true,
+            abort: None,
+        }
+    }
 }
 
 impl ScanConfig {
